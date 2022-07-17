7161055dbf702e8f88034ed1630289d5884fc8bf
Itagaki Jou <happyzardjou@gmail.com>
2022-07-17 12:22:54 +0900

    motion

---
+++ js/load.js
@@ -0,0 +1,20 @@
+$(window).on('load',function(){
+    $("#splash-logo").delay(500).fadeOut('slow');//ロゴを1.2秒でフェードアウトする記述
+    
+    //=====ここからローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+    $("#splash").delay(200).fadeOut('slow',function(){//ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
+    
+    $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
+    
+    });
+    //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+    
+    //=====ここから背景が伸びた後に動かしたいJSをまとめたい場合は
+    $('.splashbg1').on('animationend', function() { 
+    //この中に動かしたいJSを記載
+    });
+    //=====ここまで背景が伸びた後に動かしたいJSをまとめる
+    
+    });
+    
+    
